b46638fabfbe89f48db85a6dc8abc4b509bc80f1
Pan <125814787+Pan-John@users.noreply.github.com>
2023-04-27 17:01:12 +0800

    Create argumenrs.js

---
+++ argumenrs.js
@@ -0,0 +1,5 @@
+module.exports = [
+    "0x5e94B61BCa112683D18d5Ed27CebB16566E6d5ba", // NFT address
+    "0x7A81e50E0Ad45B31cC8E54A55095714F13a0c74e", // Token address
+    "0xbe02727047fADd7fe434E093e001745B42C5F49B"  // Bank address
+  ];
